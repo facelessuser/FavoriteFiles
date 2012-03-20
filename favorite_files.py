52c8daa48252d857512d00deb361b773f674ce4f
Isaac <faceless.shop@gmail.com>
2012-03-19 21:38:51 -0600

    Version 0.1.0 - First release

---
+++ favorite_files.py
@@ -0,0 +1,157 @@
+'''
+Favorite Files
+Licensed under MIT
+Copyright (c) 2012 Isaac Muse <isaacmuse@gmail.com>
+'''
+
+import sublime
+import sublime_plugin
+from os.path import join, exists, normpath, basename
+import json
+from time import time
+
+FILES = join(sublime.packages_path(), 'User', 'favorite_files.json')
+
+
+class FileList:
+    files = {}
+    last_access = 0
+    max_time = 600
+
+    @classmethod
+    def get(cls, s):
+        if cls.exists(s):
+            return normpath(cls.files[s])
+        else:
+            return None
+
+    @classmethod
+    def set(cls, s, path):
+        cls.files[s] = path
+
+    @classmethod
+    def exists(cls, s):
+        return True if s in cls.files else False
+
+    @classmethod
+    def remove(cls, s):
+        if cls.exists(s):
+            del cls.files[s]
+
+    @classmethod
+    def load(cls):
+        cls.last_access = time()
+
+    @classmethod
+    def keys(cls):
+        return [x for x in cls.files]
+
+
+def create_favorite_list(l, force=False):
+    errors = False
+    if not exists(FILES) or force:
+        try:
+            j = json.dumps(l, sort_keys=True, indent=4, separators=(',', ': '))
+            with open(FILES, 'w') as f:
+                f.write(j + "\n")
+        except:
+            sublime.error_message('Failed to create favorite_files.json')
+            errors = True
+    return errors
+
+
+def load_favorite_files(force=False):
+    errors = False
+    if not exists(FILES):
+        if create_favorite_list({}, True):
+            errors = True
+    if not errors and (force or time() - FileList.last_access > FileList.max_time):
+        with open(FILES, "r") as f:
+            content = f.read()
+        file_list = json.loads(content)
+        FileList.files = file_list
+    return errors
+
+
+class SelectFavoriteFileCommand(sublime_plugin.WindowCommand):
+    def open_file(self, value):
+        f = FileList.get(self.files[value])
+        if f != None:
+            self.window.open_file(f)
+
+    def run(self):
+        if not load_favorite_files():
+            self.files = FileList.keys()
+            if len(self.files):
+                self.window.show_quick_panel(
+                    self.files,
+                    self.open_file
+                )
+            else:
+                sublime.error_message("No favorites found! Try adding some.")
+
+
+class AddFavoriteFileCommand(sublime_plugin.WindowCommand):
+    def add(self, value):
+        if not FileList.exists(value):
+            FileList.set(value, self.name)
+            create_favorite_list(FileList.files, True)
+        else:
+            sublime.error_message("The name \"%s\" already exists!" % value)
+            self.repeat_prompt()
+
+    def repeat_prompt(self):
+        view = self.window.active_view()
+        if view != None:
+            self.window.show_input_panel(
+                "Add Favorite: ",
+                self.temp_name,
+                self.add,
+                None,
+                None
+            )
+
+    def prompt(self):
+        view = self.window.active_view()
+        if view != None:
+            self.name = view.file_name()
+            if self.name != None:
+                temp_name = basename(self.name)
+                idx = 0
+                key = temp_name
+                while FileList.exists(key):
+                    key = temp_name + ("_%d" % idx)
+                    idx += 1
+                self.temp_name = key
+                self.window.show_input_panel(
+                    "Add Favorite: ",
+                    key,
+                    self.add,
+                    None,
+                    None
+                )
+
+    def run(self):
+        self.prompt()
+
+
+class RemoveFavoriteFileCommand(sublime_plugin.WindowCommand):
+    def remove(self, value):
+        key = self.files[value]
+        if FileList.exists(key):
+            FileList.remove(key)
+            create_favorite_list(FileList.files, True)
+
+    def run(self):
+        if not load_favorite_files():
+            self.files = FileList.keys()
+            if len(self.files):
+                self.window.show_quick_panel(
+                    self.files,
+                    self.remove
+                )
+            else:
+                sublime.error_message("No favorites found!")
+
+
+load_favorite_files(True)
---
+++ readme.md
@@ -0,0 +1,29 @@
+# About
+This is a simple plugin to save favorite files that are not part of a project.  It supports adding and removing favorites and open favorite files.
+
+# Usage
+All commands are accessible via the command palatte.
+
+## Favorite Files: Open File
+Command provides a quick list to select one of your favorite files to open.
+
+## Favorite Files: Add File
+Add the current opened file to your favorites.  An input panel will be opened so you can provide a unique name for your favorite file.
+
+## Favorite Files: Remove File
+Remove a favorite file form your list.
+
+# License
+
+Favorite Files is released under the MIT license.
+
+Copyright (c) 2011 Isaac Muse <isaacmuse@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+# Version 0.1.0
+- First release
